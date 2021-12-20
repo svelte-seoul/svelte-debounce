f8b62d8ba81438a80cccb1dc6af601a35602320c
yujong-lee <yujonglee@g.skku.edu>
2021-12-20 10:55:19 +0900

    Fix type and description

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
-type Config = {delay: number, callback: (s: string) => void}
+type Config = {delay?: number, callback: (s: string) => void}
 
 declare module 'svelte-debounce' {
   const debounce: (node: Node, config: Config) => void
